1a4599c542496edbaf00e992cd9cfedb6e91dbd7
hectorLop <lopez.almazan.hector@gmail.com>
2022-08-15 11:42:36 +0200

    Added unitary tests for LocalStorage

---
+++ tests/storages/test_storages.py
@@ -0,0 +1,19 @@
+import pytest
+
+from mixver.storages.local_storage import LocalStorage
+
+
+def test_local_storage_creation():
+    assert True == False
+
+
+def test_local_storage_creation_new_folder():
+    assert True == False
+
+
+def test_local_storage_push():
+    assert True == False
+
+
+def test_local_storage_pull():
+    assert True == False
