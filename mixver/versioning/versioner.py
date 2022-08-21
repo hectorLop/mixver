aea69bc6c3352771b52a1b7dce4ec7faa5dfcaa0
hectorLop <lopez.almazan.hector@gmail.com>
2022-08-21 19:04:51 +0200

    feature: Implemented add_artifact() in Versioner()

--- mixver/versioning/versioner.py
+++ mixver/versioning/versioner.py
@@ -1,19 +1,26 @@
+import json
 import os
 from dataclasses import dataclass, field
 from pathlib import Path
+from typing import Optional
 
 
 @dataclass(frozen=True)
 class Versioner:
     """
     Class that manages the artifacts versioning
+
+    Attributes:
+        storage_path (str): Path where to create the version and tag files.
+        _version_file (str): Version filename.
+        _tags_file (str): Tags filename.
     """
 
     storage_path: str
     _version_file: str = field(default=".versions.json", init=False)
     _tags_file: str = field(default=".tags.json", init=False)
 
-    def __post_init__(self):
+    def __post_init__(self) -> None:
         """
         The post initilizer checks if the storage path already contains
         the versions and tags file. Otherwise, they are created.
@@ -26,3 +33,59 @@ class Versioner:
 
         if not os.path.isfile(tags_filepath):
             open(tags_filepath, "a").close()
+
+    def add_artifact(self, name: str, tags: list[str] = []) -> str:
+        """
+        Add an artifact to the system. In the case that the artifact already
+        exists, its version will be upgraded.
+
+        Args:
+            name (str): Artifact's name.
+            tags (list[str]): Artifact's tags. Default is []
+
+        Returns:
+            str: Artifact's filename.
+        """
+
+        with open(Path(self.storage_path, self._version_file), "r") as file:
+            try:
+                version_data = json.load(file)
+            except json.decoder.JSONDecodeError:
+                version_data = {}
+
+        hashed_name = str(hash(name))
+
+        if hashed_name in version_data:
+            versions = version_data[hashed_name].keys()
+            versions = list(map(int, versions))
+            new_version = max(versions) + 1
+        else:
+            new_version = 1
+            version_data[hashed_name] = {}
+
+        filename = f"{hashed_name}_{new_version}"
+        version_data[hashed_name][new_version] = filename
+
+        with open(Path(self.storage_path, self._version_file), "w") as file:
+            json.dump(version_data, file)
+
+        if tags:
+            with open(self._tags_file, "r") as file:
+                tags_data = json.load(file)
+
+            for tag in tags:
+                if tag not in data:
+                    tags_data[tag] = {tag: {hashed_name: {new_version: filename}}}
+                else:
+                    tags_data[tag] = {hashed_name: {new_version: filename}}
+
+        return filename
+
+    def update(self):
+        pass
+
+    def remove(self):
+        pass
+
+    def get(self):
+        pass
--- tests/versioning/test_versioner.py
+++ tests/versioning/test_versioner.py
@@ -50,3 +50,62 @@ def test_versioner_existant_storage():
         assert data["key"] == 1
 
     shutil.rmtree(storage_path)
+
+
+def test_versioner_add_new_artifact():
+    """
+    Test adding a new artifact
+    """
+    storage_path = Path(ROOT, "prueba_versioner")
+    os.makedirs(storage_path)
+
+    versioner = Versioner(storage_path=storage_path)
+    filename = versioner.add_artifact("artifact")
+
+    hashed_name, version = filename.split("_")
+
+    assert hashed_name == str(hash("artifact"))
+    assert version == "1"
+
+    with open(Path(storage_path, ".versions.json"), "r") as file:
+        data = json.load(file)
+
+        assert hashed_name in data
+        assert version in data[hashed_name]
+        assert data[hashed_name][version] == filename
+
+    shutil.rmtree(storage_path)
+
+
+def test_versioner_add_existing_artifact():
+    """
+    Test adding a new artifact
+    """
+    storage_path = Path(ROOT, "prueba_versioner")
+    os.makedirs(storage_path)
+
+    versioner = Versioner(storage_path=storage_path)
+    expected_hashed_name = str(hash("artifact"))
+    old_version = "1"
+    data = {
+        expected_hashed_name: {old_version: f"{expected_hashed_name}_{old_version}"}
+    }
+
+    with open(Path(storage_path, ".versions.json"), "w") as file:
+        json.dump(data, file)
+
+    filename = versioner.add_artifact("artifact")
+
+    hashed_name, version = filename.split("_")
+
+    assert hashed_name == expected_hashed_name
+    assert version == "2"
+
+    with open(Path(storage_path, ".versions.json"), "r") as file:
+        data = json.load(file)
+
+        assert hashed_name in data
+        assert version in data[hashed_name]
+        assert data[hashed_name][version] == filename
+
+    shutil.rmtree(storage_path)
